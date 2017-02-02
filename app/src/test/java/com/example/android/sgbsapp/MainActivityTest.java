f49a5e4c3fcb301adb515c664ed4dd9b05626e23
Djibril <djibsone51@gmail.com>
2017-02-02 15:34:08 +0000

    first commit

---
+++ app/src/test/java/com/example/android/sgbsapp/MainActivityTest.java
@@ -0,0 +1,16 @@
+package com.example.android.sgbsapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Djibril on 1/26/2017.
+ */
+public class MainActivityTest {
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+}
